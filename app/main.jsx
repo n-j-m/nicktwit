89796c090f7dc7a0b95c6b02ee4b4c2046fcfbd5
n-j-m <nick65@gmail.com>
2015-02-23 19:01:46 -0600

    initial

---
+++ app/main.jsx
@@ -0,0 +1,13 @@
+"use strict";
+
+require("babel/register");
+
+import React from "react";
+import Router from "react-router";
+
+import routes from "./routes";
+
+const content = document.getElementById("content");
+Router.run(routes, Router.HistoryLocation, (Handler) => {
+  return React.render(<Handler/>, content);
+});
